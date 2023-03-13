c3181eeeeeae4987050524d1203a0cce6edb5629
Irfan Aziz <irfan.aziz@rumahsiapkerja.com>
2023-03-14 06:33:36 +0700

    add post page

---
+++ components/SectionHeader.js
@@ -0,0 +1,3 @@
+export default function SectionHeader({ children }) {
+  return <h1 className="text-[34px] text-center">{children}</h1>;
+}
---
+++ pages/posts.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Layout from "../components/Layout";
+import CardPost from "@/components/CardPost";
+
+import mockPosts from "../utils/posts.json";
+import SectionHeader from "../components/SectionHeader";
+
+export default function Posts() {
+  const [posts, setPosts] = useState(mockPosts);
+
+  return (
+    <Layout>
+      <section className="container mx-auto px-4 md:px-0">
+        <SectionHeader>UI design</SectionHeader>
+
+        {posts.length ? (
+          <div className="flex flex-wrap -mx-4 mt-[60px]">
+            {posts &&
+              posts.map((post) => (
+                <div className="w-full md:w-1/3 px-4 py-6">
+                  <CardPost {...post} />
+                </div>
+              ))}
+          </div>
+        ) : (
+          <div className="text-center py-20">
+            <h2 className="text-[64px]">No result 😥</h2>
+            <p className="md:w-1/2 mx-auto text-[20px] leading-[30px] text-white/60">
+              We couldn’t find any posts with the keyword `yahahahayuk`. Please
+              try another keyword.
+            </p>
+          </div>
+        )}
+      </section>
+    </Layout>
+  );
+}
