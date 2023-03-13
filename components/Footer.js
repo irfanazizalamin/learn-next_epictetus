7d22215eebdd6f020d42f62459d2b4d0fe7165d0
Irfan Aziz <irfan.aziz@rumahsiapkerja.com>
2023-03-14 06:30:13 +0700

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="py-16">
+      <div className="container mx-auto">
+        <p className="text-center text-[16px] text-white/60">
+          Copyright (c) 2021 - Design By Nauval
+        </p>
+      </div>
+    </footer>
+  );
+}
